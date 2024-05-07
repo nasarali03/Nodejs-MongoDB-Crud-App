4f8c7dda90e4a58cc5a6576aadcf364c733e7d5c
nasarali03 <nasarali1805@gmail.com>
2024-05-07 23:27:35 +0500

    Product model

---
+++ src/model/product.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = new mongoose.model("Product", productSchema);
+export default Product;
